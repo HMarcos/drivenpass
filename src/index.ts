aaf154be99765f31941b684077e0173131e7b6ff
Marcos Henrique <marcosmarcone48@gmail.com>
2022-07-16 16:08:40 -0300

    Initial configuration

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { json } from "express";
+import "express-async-errors";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors);
+app.use(json)
+
+
+const PORT = +process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`The server is running on PORT ${PORT}...`);
+});
