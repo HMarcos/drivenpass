174f8d82de171d987d63202932987e4b831637ff
Marcos Henrique <marcosmarcone48@gmail.com>
2022-07-16 21:24:16 -0300

    FEAT (sign-in): validate the request body with joi.
    Make sure that the request body has the email and password properties.

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { signIn, signUp } from "../controllers/authController.js";
 import validateSchema from "../middlewares/validateSchemaMiddleware.js";
+import signInSchema from "../schemas/signInSchema.js";
 import signUpSchema from "../schemas/signUpSchema.js";
 
 const authRouter = Router();
 
 authRouter.post('/sign-up', validateSchema(signUpSchema), signUp);
-authRouter.post('/sign-in', signIn);
+authRouter.post('/sign-in', validateSchema(signInSchema), signIn);
 
 export default authRouter;
