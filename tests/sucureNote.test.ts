23010b8a8ba3521c6cf3af64c5fd5845be114879
Marcos Henrique <marcosmarcone48@gmail.com>
2022-07-22 11:47:39 -0300

    TEST: configure more tests to secure note endpoint.

--- tests/sucureNote.test.ts
+++ tests/sucureNote.test.ts
@@ -23,7 +23,68 @@ describe("Secure Notes test suite", () => {
             where: { title: newSecureNote.title }
         });
         expect(savedSecureNote).not.toBeNull();
-    })
+    });
+
+    it("get all secure notes for admin user", async () => {
+        let response = await supertest(app).get('/sign-in').send(admin_user);
+        const token = response.body.token;
+
+        let newSecureNote = secureNoteFactory.createSecureNoteInfo();
+        await supertest(app).post('/secure-notes').send(newSecureNote).set("Authorization", `Bearer ${token}`);
+
+        newSecureNote = secureNoteFactory.createSecureNoteInfo();
+        await supertest(app).post('/secure-notes').send(newSecureNote).set("Authorization", `Bearer ${token}`);
+
+        response = await supertest(app).get(`/secure-notes`).set("Authorization", `Bearer ${token}`);
+        expect(response.body).not.toBeNull();
+        expect(response.body).toHaveLength(2);
+
+    });
+
+    it("get a secure note for admin user", async () => {
+        let response = await supertest(app).get('/sign-in').send(admin_user);
+        const token = response.body.token;
+
+        const newSecureNote = secureNoteFactory.createSecureNoteInfo();
+        await supertest(app).post('/secure-notes').send(newSecureNote).set("Authorization", `Bearer ${token}`);
+
+        const savedSecureNote = await prismaClient.secureNote.findFirst({
+            where: {
+                title: newSecureNote.title
+            }
+        });
+
+        response = await supertest(app).get(`/secure-notes/${savedSecureNote.id}`).set("Authorization", `Bearer ${token}`);
+        expect(response.body).not.toBeNull();
+
+    });
+
+    it("delete a secure note for admin user", async () => {
+        let response = await supertest(app).get('/sign-in').send(admin_user);
+        const token = response.body.token;
+
+        const newSecureNote = secureNoteFactory.createSecureNoteInfo();
+        await supertest(app).post('/secure-notes').send(newSecureNote).set("Authorization", `Bearer ${token}`);
+
+        const savedSecureNote = await prismaClient.secureNote.findFirst({
+            where: {
+                title: newSecureNote.title
+            }
+        });
+
+        response = await supertest(app).delete(`/secure-notes/${savedSecureNote.id}`).set("Authorization", `Bearer ${token}`);
+        expect(response.status).toBe(200);
+
+    });
+
+    it("Get an invalid secure note, error 404", async () => {
+        let response = await supertest(app).get('/sign-in').send(admin_user);
+        const token = response.body.token;
+
+        response = await supertest(app).get(`/secure-notes/100`).set("Authorization", `Bearer ${token}`);
+        expect(response.status).toBe(404);
+
+    });
 });
 
 afterAll(async () => {
